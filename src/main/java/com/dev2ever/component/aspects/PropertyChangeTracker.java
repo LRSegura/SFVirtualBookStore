8a8da52fe90c77f791eb7ad05ca7b6403500cd81
Luis Rafael Segura <lrseguramartinez@gmail.com>
2024-10-17 14:58:25 -0400

    Adding Aspects features

--- src/main/java/com/dev2ever/component/aspects/PropertyChangeTracker.java
+++ src/main/java/com/dev2ever/component/aspects/PropertyChangeTracker.java
@@ -1,6 +1,7 @@
 package com.dev2ever.component.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,15 +15,28 @@ public class PropertyChangeTracker {
     Logger logger = Logger.getLogger(PropertyChangeTracker.class.getName());
 
     @Before("execution(void com.dev2ever.service.*.*(..))")
-    public void trackChange(){
-        logger.info("Tracking call method");
+    public void trackCallMethodBefore(){
+        logger.info("Tracking. Before call method");
     }
 
-    @Before("execution(void com.dev2ever.model.*.set(..))")
-    public void trackChangeWithParam(JoinPoint joinPoint){
+//    @Before("execution(void com.dev2ever.model.*.set(..))")
+//    public void trackChangeWithParamEntities(JoinPoint joinPoint){
+//        String methodName = joinPoint.getSignature().getName();
+//        Object value = joinPoint.getArgs()[0];
+//        String message = String.format("method name %s. Value %s", methodName, value);
+//        logger.info(message);
+//    }
+
+    @Before("execution(void com.dev2ever.service.*.*(..))")
+    public void trackChangeWithParamServices(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         Object value = joinPoint.getArgs()[0];
-        String message = String.format("method name %s. Value %s", methodName, value);
+        String message = String.format("Method name: %s. Value: %s", methodName, value);
         logger.info(message);
+    }
+
+    @After("execution(void com.dev2ever.service.*.*(..))")
+    public void trackCallMethodAfter(){
+        logger.info("Tracking. After call method");
     }
 }
